12ce4714f3e3b09443ac77aafd8403723512a307
Алексей <fuzzylolpro@gmail.com>
2023-06-12 19:20:31 +0300

    потоки данных

--- src/HomeWork/Lesson14/Main2.java
+++ src/HomeWork/Lesson14/Main2.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Main2 {
     public static void main(String[] args) throws IOException {
-        try (FileReader fr = new FileReader("B:\\romeo-and-juliet.txt")) {
+        try (FileReader fr = new FileReader("D:\\romeo-and-juliet.txt")) {
             int i;
             while ((i = fr.read()) != -1) {
                 System.out.print((char) i);
@@ -13,11 +13,11 @@ public class Main2 {
             e.printStackTrace();
         }
         try {
-            BufferedReader br = new BufferedReader(new FileReader("B:\\romeo-and-juliet.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("D:\\romeo-and-juliet.txt"));
             String line = br.readLine();
             String longestWord = "";
             while (line != null) {
-                String[] words = line.split("\\s");
+                String[] words = line.split("\\W");
                 for (String word : words) {
                     if (word.length() > longestWord.length()) {
                         longestWord = word;
@@ -28,7 +28,7 @@ public class Main2 {
                 }
                 line = br.readLine();
             }
-            FileWriter fw = new FileWriter("B:\\lengthsWord.txt");
+            FileWriter fw = new FileWriter("D:\\lengthsWord.txt");
             fw.write(longestWord);
             fw.close();
             br.close();
