b48ac87fa5c709e3d8d05e32cd6961fe4383f1a2
AlexeyKuznechick <fuzzylolpro@gmail.com>
2023-06-21 10:49:10 +0300

    Убрал логический блок, сплитанул значение и строку, перевожу 1-ое значение строки в USD

--- src/HomeWork/Lesson17/task3.java
+++ src/HomeWork/Lesson17/task3.java
@@ -7,13 +7,10 @@ public class task3 {
     public static void main(String[] args) {
         System.out.println("Введите сумму в BYN:");
         Scanner scanner = new Scanner(System.in);
-        try {
-            String money =scanner.nextLine();
-            Function<String,Double> fl = i -> Double.parseDouble(money);{
-                System.out.println(fl.apply(money) * 0.3352 + " USD");
-            }
-        } catch (Exception e){
-            System.out.println("Неверная сумма");
+        String money = scanner.nextLine();
+        Function<String, Double> fl = i -> Double.parseDouble(money.split(" ")[0]);
+        {
+            System.out.println(fl.apply(money) * 0.3352 + " USD");
         }
     }
 }
