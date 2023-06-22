d33eb5b98047dfda5a2a104756457aa4459c49f9
AlexeyKuznechick <fuzzylolpro@gmail.com>
2023-06-22 17:26:37 +0300

    Чтото сделал все испортил:D

--- src/HomeWork/Lesson18/Developer1.java
+++ src/HomeWork/Lesson18/Developer1.java
@@ -1,5 +1,7 @@
 package HomeWork.Lesson18;
 
+import java.util.Objects;
+
 public class Developer1 {
     private String name = "Anna";
     private String name1 = "Alla";
@@ -9,6 +11,19 @@ public class Developer1 {
 
     public String getName1() {
         return name1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Developer1 that = (Developer1) o;
+        return id == that.id && id1 == that.id1 && Objects.equals(name, that.name) && Objects.equals(name1, that.name1) && Objects.equals(name3, that.name3);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, name1, name3, id, id1);
     }
 
     public void setName1(String name1) {
@@ -25,6 +40,17 @@ public class Developer1 {
 
     public int getId1() {
         return id1;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer1{" +
+                "name='" + name + '\'' +
+                ", name1='" + name1 + '\'' +
+                ", name3='" + name3 + '\'' +
+                ", id=" + id +
+                ", id1=" + id1 +
+                '}';
     }
 
     public void setId1(int id1) {
@@ -45,12 +71,5 @@ public class Developer1 {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Developer1{" +
-                "name='" + name + '\'' +
-                '}';
     }
 }
--- src/HomeWork/Lesson18/LessonTask_3.java
+++ src/HomeWork/Lesson18/LessonTask_3.java
@@ -11,8 +11,8 @@ import java.util.stream.Stream;
 public class LessonTask_3 {
     public static void main(String[] args) {
         Developer1 developer1 = new Developer1();
-        Arrays.stream(new Developer1[]{developer1}).filter(developer11 -> developer11.getName().startsWith("An") )
-                .filter(developer11 -> developer11.getId()>10) // не печатает ид
+        Stream.of(developer1).filter(developer11 -> developer11.getName().startsWith("An"))
+                .filter(developer12 -> developer12.hashCode()>10) // не печатает ид
                 .forEach(System.out::println);
     }
 }
